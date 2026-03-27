23e50d0fa451ccc5ef838844622c6935a4b947e1
Gabrielgsd <104272301+GabrielgsdCIUwU@users.noreply.github.com>
2026-03-27 13:50:26 +0100

    feat(pages): implement login page with 2FA support

---
+++ lib/presentation/pages/login_page.dart
@@ -0,0 +1,64 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_riverpod/flutter_riverpod.dart';
+import 'package:vrcma/core/errors/failure.dart';
+import 'package:vrcma/presentation/services/snackbar_service.dart';
+import 'package:vrcma/presentation/state/auth_provider.dart';
+import 'package:vrcma/presentation/widgets/auth_header.dart';
+import 'package:vrcma/presentation/widgets/login_form.dart';
+import 'package:vrcma/presentation/widgets/otp_form.dart';
+class LoginPage  extends ConsumerStatefulWidget{
+  const LoginPage({super.key});
+
+  @override
+  ConsumerState<LoginPage> createState() => _LoginPageState();
+}
+
+class _LoginPageState extends ConsumerState<LoginPage> {
+  bool _showOtpView = false;
+
+  @override
+  Widget build(BuildContext context) {
+   final authState = ref.watch(authStateProvider);
+
+   ref.listen(authStateProvider, (prev, next) {
+     next.whenOrNull(
+       error: (error, _) {
+         if (error is TwoFactorRequiredFailure) {
+           setState(() => _showOtpView = true);
+         } else if (error is Failure) {
+           ref.read(snackbarServiceProvider).show(error.message, context);
+         }
+       },
+       data: (user) {
+         if (user != null) ref.read(snackbarServiceProvider).show('Login Success!', context);
+       },
+     );
+   });
+
+   return Scaffold(
+     body: Center(
+       child: SingleChildScrollView(
+         padding: const EdgeInsets.all(24),
+         child: Column(
+           children: [
+             const AuthHeader(),
+             const SizedBox(height: 40),
+
+             if (!_showOtpView)
+               LoginForm(
+                   isLoading: authState.isLoading,
+                   onLogin: (user, pass) => ref.read(authStateProvider.notifier)
+                    .login(user, pass),
+               ) else
+                 OtpForm(
+                     isLoading: authState.isLoading,
+                     onVerify: (code) => ref.read(authStateProvider.notifier).verifyOtp(code),
+                     onCancel: () => setState(() => _showOtpView = false)
+                 ),
+           ],
+         ),
+       ),
+     )
+   );
+  }
+}
