ca3a36458c060ee37217b7f128cfa60a80a70159
Gabrielgsd <104272301+GabrielgsdCIUwU@users.noreply.github.com>
2026-03-27 21:02:39 +0100

    feat(automation): add InvitationType and related notification entities

---
+++ lib/domain/entities/automation/invitation_type.dart
@@ -0,0 +1,31 @@
+import 'package:equatable/equatable.dart';
+import 'package:flutter/animation.dart';
+import 'package:vrchat_dart/vrchat_dart.dart';
+
+/// Represents an incoming invitation request from VRChat.
+class InvitationType extends Equatable {
+  final String id;
+  final String senderId;
+  final String senderName;
+  final List<String> senderTags;
+  
+  const InvitationType({
+    required this.id,
+    required this.senderId,
+    required this.senderName,
+    required this.senderTags,
+  });
+  
+  @override
+  List<Object?> get props => [id, senderId, senderName, senderTags];
+}
+
+/// When someone wants to join your world.
+class RequestInvite extends InvitationType {
+  const RequestInvite({required super.id, required super.senderId, required super.senderName, required super.senderTags});
+}
+
+/// When someone invites you to join their world.
+class InviteReceived extends InvitationType {
+  const InviteReceived({required super.id, required super.senderId, required super.senderName, required super.senderTags});
+}
