1275423eaa260b006b5ea3163cbe2ce44d7dc7a4
Gabrielgsd <104272301+GabrielgsdCIUwU@users.noreply.github.com>
2026-03-27 21:02:49 +0100

    feat(automation): define automation repository interface

---
+++ lib/domain/repositories/i_automation_repository.dart
@@ -0,0 +1,19 @@
+import 'package:vrcma/domain/entities/automation/invitation_type.dart';
+
+abstract class IAutomationRepository {
+  /// Stream that emits new invitation requests in real-time.
+  Stream<InvitationType> watchInvitations();
+  
+  /// Accepts a specific request invitation.
+  Future<void> acceptRequestInvitation(RequestInvite requestInvite, int? slot);
+  
+  /// Accepts a specific invite
+  Future<void> acceptInvitation(InviteReceived invite, int? slot);
+  
+  /// Rejects a notification (invite or request invite)
+  Future<void> rejectNotificationWithMessage(InvitationType notification, int? slot);
+
+  /// Dismisses a notification (invite or request invite) without response
+  Future<void> dismissNotification(InvitationType notification);
+}
+
