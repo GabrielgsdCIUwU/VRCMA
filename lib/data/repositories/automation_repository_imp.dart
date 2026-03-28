c142512e2483760264fb0f088318048bbc934206
Gabrielgsd <104272301+GabrielgsdCIUwU@users.noreply.github.com>
2026-03-28 13:10:21 +0100

    feat(automation): implement AutomationRepository for managing invitations

---
+++ lib/data/repositories/automation_repository_imp.dart
@@ -0,0 +1,90 @@
+import 'dart:async';
+import 'package:vrchat_dart/vrchat_dart.dart';
+import 'package:vrcma/domain/entities/automation/invitation_type.dart';
+import 'package:vrcma/domain/repositories/i_automation_repository.dart';
+
+class AutomationRepositoryImp implements IAutomationRepository {
+  final VrchatDart _vrcApi;
+  
+  AutomationRepositoryImp(this._vrcApi);
+  
+  @override
+  Stream<InvitationType> watchInvitations() {
+    return _vrcApi.streaming.vrcEventStream
+        .where((event) => event is NotificationReceivedEvent)
+        .cast<NotificationReceivedEvent>()
+        .where((event) =>
+          event.notification.type == NotificationType.invite ||
+          event.notification.type == NotificationType.requestInvite)
+        .asyncMap((event) async {
+        final notification = event.notification;
+        final userResponse = await _vrcApi.rawApi.getUsersApi()
+              .getUser(userId: notification.senderUserId);
+        
+        var userData = userResponse.data;
+        
+        if (notification.type == NotificationType.requestInvite) {
+          return RequestInvite(
+            id: notification.id,
+            senderId: notification.senderUserId,
+            senderName: userData?.displayName ?? notification.senderUserId,
+            senderTags: userData?.tags ?? [],
+          );
+        }
+        return InviteReceived(
+          id: notification.id,
+          senderId: notification.senderUserId,
+          senderName: userData?.displayName ?? notification.senderUserId,
+          senderTags: userData?.tags ?? [],
+        );
+    });
+  }
+  @override
+  Future<void> acceptRequestInvitation(RequestInvite requestInvite, int? slot) async {
+    final response = await _vrcApi.rawApi.getAuthenticationApi().getCurrentUser();
+    final currentUser = response.data;
+    
+    if (currentUser == null) return;
+    
+    final presence = currentUser.presence;
+    final world = presence?.world;
+    final instanceId = presence?.instance;
+    
+    if (presence == null || world == null || world == 'offline' || world.isEmpty) return;
+    
+    
+    await _vrcApi.rawApi.getInviteApi().inviteUser(
+      userId: requestInvite.senderId,
+      inviteRequest: InviteRequest(
+        instanceId: "$world:$instanceId",
+        messageSlot: slot
+      )
+    );
+    
+    await dismissNotification(requestInvite);
+  }
+  
+  @override
+  Future<void> acceptInvitation(InviteReceived invite) async {
+    await _vrcApi.rawApi.getNotificationsApi().acceptFriendRequest(
+        notificationId: invite.id
+    );
+
+    await dismissNotification(invite);
+  }
+  
+  @override
+  Future<void> rejectNotificationWithMessage(InvitationType notification, int slot) async {
+    await _vrcApi.rawApi.getInviteApi().respondInvite(
+        notificationId: notification.id,
+        inviteResponse: InviteResponse(responseSlot: slot)
+    );
+  }
+  
+  @override
+  Future<void> dismissNotification(InvitationType notification) async {
+    await _vrcApi.rawApi.getNotificationsApi().deleteNotification(
+        notificationId: notification.id
+    );
+  }  
+}
