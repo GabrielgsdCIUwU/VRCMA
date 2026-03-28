3d3ea054cdeb35e2f6d4af20f153cfe062dc91de
Gabrielgsd <104272301+GabrielgsdCIUwU@users.noreply.github.com>
2026-03-28 21:00:59 +0100

    feat(auth): add location, status, and avatarUrl to VrcUser entity

--- lib/domain/entities/auth/vrc_user.dart
+++ lib/domain/entities/auth/vrc_user.dart
@@ -5,15 +5,21 @@ class VrcUser extends Equatable {
   final String displayName;
   final String? bio;
   final List<String> tags;
+  final String location; // ID World or offline if not friends
+  final String status; // Online, Join me, Ask me, Offline
+  final String avatarUrl;
 
   const VrcUser({
     required this.id,
     required this.displayName,
     this.bio,
     required this.tags,
+    this.location = '',
+    this.status = '',
+    this.avatarUrl = ''
   });
 
   @override
-  List<Object?> get props => [id, displayName, bio, tags];
+  List<Object?> get props => [id, displayName, bio, tags, location, status, avatarUrl];
 }
 
