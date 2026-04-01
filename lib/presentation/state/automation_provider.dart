46fb1dabddf3646dfdace97e20bd18e933046533
Gabrielgsd <104272301+GabrielgsdCIUwU@users.noreply.github.com>
2026-04-01 15:38:32 +0200

    feat(automation): implement invitation listening and processing logic in automation provider

--- lib/presentation/state/automation_provider.dart
+++ lib/presentation/state/automation_provider.dart
@@ -1,7 +1,10 @@
 import 'package:riverpod_annotation/riverpod_annotation.dart';
 import 'package:vrcma/core/di/database_provider.dart';
+import 'package:vrcma/data/repositories/automation_repository_imp.dart';
+import 'package:vrcma/domain/entities/automation/filter_profile.dart';
 import 'package:vrcma/domain/usecases/automation/process_invitation_use_case.dart';
 import 'package:vrcma/presentation/state/auth_provider.dart';
+import 'package:vrcma/presentation/state/profile_management_provider.dart';
 
 part 'automation_provider.g.dart';
 
@@ -13,6 +16,39 @@ class AutomationState extends _$AutomationState {
   }
   
   void _listenToInvitations() async {
-    ref.read(authRepositoryProvider);
+    final api = await ref.watch(vrcApiProvider.future);
+    final automationRepo = AutomationRepositoryImp(api);
+    final localRepo = await ref.watch(localSocialRepositoryProvider.future);
+    
+    final useCase = ProcessInvitationUseCase(
+      roleExtractor: UserRoleExtractor(),
+      ruleSorter: RuleSorter(),
+      ruleEvaluator: RuleEvaluator(),
+      defaultResolver: DefaultActionResolver()
+    );
+    
+    automationRepo.watchInvitations().listen((invitation) async {
+      final profiles = await ref.read(profileManagementProviderProvider.future);
+      final activeProfile = profiles.firstWhere((p) => 
+          p.isActive, 
+          orElse: () => const FilterProfile(name: 'None'));
+      
+      if (activeProfile.id == null) return;
+      
+      final userRoles = await localRepo.getRolesForUser(invitation.senderId);
+      
+      final decision = useCase.execute(
+        request: invitation,
+        profile: activeProfile,
+        userAssignedRoles: userRoles
+      );
+      
+      //TODO: Add execution
+      if (decision == RuleAction.accept) {
+        print("Accept: ${invitation.senderName}");
+      } else if (decision == RuleAction.reject) {
+        print("Reject: ${invitation.senderName}");
+      }
+    });
   }
 }
