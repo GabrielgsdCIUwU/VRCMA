fe599b8b4f0c5cc7769a3bc3cf2aefa91fba61c3
Gabrielgsd <104272301+GabrielgsdCIUwU@users.noreply.github.com>
2026-04-10 11:13:05 +0200

    feat(automation): add optional custom message to ProfileRule entity

---
+++ lib/domain/repositories/i_message_repository.dart
@@ -0,0 +1,9 @@
+import 'package:vrcma/domain/entities/automation/vrc_message.dart';
+
+abstract class IMessageRepository {
+  Future<List<CustomMessage>> getMessagesByType(VrcMessageType type);
+  Future<int> saveMessage(CustomMessage message);
+  Future<void> deleteMessage(int id);
+  Future<void> updateSlot(int messageId, int? slotIndex);
+  Future<List<CustomMessage>> getActiveSlots(VrcMessageType type);
+}
