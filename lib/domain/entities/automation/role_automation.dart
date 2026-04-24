e688a95dff6e5a2fbfa7f34132f4f5aad01b900c
Gabrielgsd <104272301+GabrielgsdCIUwU@users.noreply.github.com>
2026-04-24 10:56:47 +0200

    feat(automation): add RoleAutomation entity and AutomationTrigger enum

---
+++ lib/domain/entities/automation/role_automation.dart
@@ -0,0 +1,24 @@
+import 'package:equatable/equatable.dart';
+import 'package:vrcma/domain/entities/automation/filter_profile.dart';
+
+enum AutomationTrigger {
+  newFriend,
+  hasTag
+}
+
+class RoleAutomation extends Equatable {
+  final int? id;
+  final List<Role> roles;
+  final AutomationTrigger trigger;
+  final String? targetValue;
+
+  const RoleAutomation({
+    this.id,
+    required this.roles,
+    required this.trigger,
+    this.targetValue,
+  });
+
+  @override
+  List<Object?> get props => [id, roles, trigger, targetValue];
+}
