b9a6f4863849222c872e5ec5443be6d27a235f9e
Gabrielgsd <104272301+GabrielgsdCIUwU@users.noreply.github.com>
2026-04-24 20:20:19 +0200

    feat(social): add getFavoriteGroups to ISocialRepository

--- lib/domain/repositories/i_social_repository.dart
+++ lib/domain/repositories/i_social_repository.dart
@@ -1,7 +1,9 @@
 import 'package:dartz/dartz.dart';
 import 'package:vrcma/core/errors/failure.dart';
 import 'package:vrcma/domain/entities/auth/vrc_user.dart';
+import 'package:vrcma/domain/entities/social/favorite_group.dart';
 
 abstract class ISocialRepository {
   Future<Either<Failure, List<VrcUser>>> getFriends();
+  Future<Either<Failure, List<FavoriteGroup>>> getFavoriteGroups();
 }
