b9e7dc4fae205f04704b5a0653a4d7e1db052b7d
Gabrielgsd <104272301+GabrielgsdCIUwU@users.noreply.github.com>
2026-04-24 16:42:05 +0200

    feat(friends): implement friend automation processing in friends provider

--- lib/presentation/state/friends_provider.dart
+++ lib/presentation/state/friends_provider.dart
@@ -1,6 +1,8 @@
 import 'package:riverpod_annotation/riverpod_annotation.dart';
+import 'package:vrcma/core/di/database_provider.dart';
 import 'package:vrcma/data/repositories/social_repository_imp.dart';
 import 'package:vrcma/domain/entities/auth/vrc_user.dart';
+import 'package:vrcma/domain/usecases/automation/process_friend_automations_use_case.dart';
 import 'package:vrcma/presentation/state/auth_provider.dart';
 
 part 'friends_provider.g.dart';
@@ -15,11 +17,17 @@ class FriendsList extends _$FriendsList {
   Future<List<VrcUser>> _fetchFriends() async {
     final api = await ref.watch(vrcApiProvider.future);
     final repo = SocialRepositoryImp(api);
+    final localSocialRepo = await ref.watch(localSocialRepositoryProvider.future);
 
     final result = await repo.getFriends();
     return result.fold(
           (failure) => throw failure.message,
-          (friends) => friends,
+          (friends) async {
+            final useCase = ProcessFriendAutomationsUseCase(localSocialRepo);
+            await useCase.execute(friends);
+            
+            return friends;
+          },
     );
   }
   
