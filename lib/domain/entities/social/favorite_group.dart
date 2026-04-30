51c4c464b9cb7a7b39c42547cca31115e832376d
Gabrielgsd <104272301+GabrielgsdCIUwU@users.noreply.github.com>
2026-04-30 19:49:36 +0200

    feat(social): add friendIds to FavoriteGroup entity

--- lib/domain/entities/social/favorite_group.dart
+++ lib/domain/entities/social/favorite_group.dart
@@ -3,12 +3,14 @@ import 'package:equatable/equatable.dart';
 class FavoriteGroup extends Equatable {
   final String id;
   final String name;
+  final List<String> friendIds;
   
   const FavoriteGroup({
     required this.id,
     required this.name,
+    this.friendIds = const [],
   });
   
   @override
-  List<Object?> get props => [id, name];
+  List<Object?> get props => [id, name, friendIds];
 }
