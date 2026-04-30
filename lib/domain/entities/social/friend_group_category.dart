390e6a54a6c48ff4af6e2c0dcf7c987626e6897b
Gabrielgsd <104272301+GabrielgsdCIUwU@users.noreply.github.com>
2026-04-30 20:31:59 +0200

    feat(social): add subcategories and total friends count to FriendGroupCategory

--- lib/domain/entities/social/friend_group_category.dart
+++ lib/domain/entities/social/friend_group_category.dart
@@ -6,11 +6,21 @@ class FriendGroupCategory {
   final String title;
   final IconData icon;
   final List<VrcUser> friends;
+  final List<FriendGroupCategory> subCategories;
   
   const FriendGroupCategory({
     required this.id,
     required this.title,
     required this.icon,
-    required this.friends,
+    this.friends = const [],
+    this.subCategories = const [],
   });
+  
+  int get totalFriendsCount {
+    int count = friends.length;
+    for (var subCategory in subCategories) {
+      count += subCategory.totalFriendsCount;
+    }
+    return count;
+  }
 }
