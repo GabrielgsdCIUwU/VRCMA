065c8836572bc83e7e139b881751b732ac517626
Gabrielgsd <104272301+GabrielgsdCIUwU@users.noreply.github.com>
2026-05-06 19:36:40 +0200

    test(mocks): add repositories to generated mocks

--- test/helpers/test_mocks.dart
+++ test/helpers/test_mocks.dart
@@ -2,10 +2,13 @@ import 'package:mockito/annotations.dart';
 import 'package:cookie_jar/cookie_jar.dart';
 import 'package:vrchat_dart/vrchat_dart.dart';
 import 'package:dio/dio.dart';
+import 'package:vrcma/domain/repositories/i_auth_repository.dart';
 import 'package:vrcma/domain/repositories/i_automation_repository.dart';
 import 'package:vrcma/domain/repositories/i_local_social_repository.dart';
+import 'package:vrcma/domain/repositories/i_message_repository.dart';
 import 'package:vrcma/domain/repositories/i_profile_repository.dart';
 import 'package:vrcma/domain/repositories/i_log_repository.dart';
+import 'package:vrcma/domain/repositories/i_social_repository.dart';
 import 'package:vrcma/domain/usecases/automation/message_slot_manager.dart';
 import 'package:vrcma/domain/usecases/automation/process_invitation_use_case.dart';
 
@@ -14,6 +17,9 @@ import 'package:vrcma/domain/usecases/automation/process_invitation_use_case.dar
   AuthenticationApi,
   CookieJar,
   Dio,
+  IAuthRepository,
+  IMessageRepository,
+  ISocialRepository,
   IAutomationRepository,
   ILocalSocialRepository,
   IProfileRepository,
