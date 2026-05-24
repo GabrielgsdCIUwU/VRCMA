c6b2710495829f46dc201f4df57ccba7cdbce14a
Gabrielgsd <104272301+GabrielgsdCIUwU@users.noreply.github.com>
2026-05-24 11:45:50 +0200

    refactor(logs): update import to use local_storage_provider instead of database_provider

--- lib/presentation/state/logs_provider.dart
+++ lib/presentation/state/logs_provider.dart
@@ -1,5 +1,5 @@
 import 'package:riverpod_annotation/riverpod_annotation.dart';
-import 'package:vrcma/core/di/database_provider.dart';
+import 'package:vrcma/core/di/local_storage_provider.dart';
 import 'package:vrcma/domain/entities/automation/automation_log.dart';
 
 part 'logs_provider.g.dart';
