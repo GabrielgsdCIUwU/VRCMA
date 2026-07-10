08048e7a35feb5b909f4a75fa4f642dfff105742
GabrielgsdCIUwU <gsuarezdominguez@deloitte.es>
2026-07-10 08:49:08 +0200

    feat(tests): update database service tests to include all expected tables and correct initial data assertions

--- test/core/database/database_service_test.dart
+++ test/core/database/database_service_test.dart
@@ -21,16 +21,21 @@ void main() {
       final dbService = DatabaseService();
       final database = await dbService.database;
       
-      final tables = [
+      final expectedTables = [
+        'app_configurations',
         'roles',
         'vrc_users',
+        'custom_messages',
         'profiles',
         'logs',
         'friend_roles',
-        'profile_rules'
+        'profile_rules',
+        'role_automations',
+        'automation_assigned_roles',
+        'status_profiles',
+        'status_rules',
       ];
-      
-      for (var table in tables) {
+      for (var table in expectedTables) {
         final result = await database.rawQuery(
           "SELECT name FROM sqlite_master WHERE type='table' AND name=?",
           [table]
@@ -39,13 +44,16 @@ void main() {
       }
     });
     
-    test('Initial dummy data should be inserted (VIP, Admin, Blocked)', () async {
+    test('Initial seeded configurations and roloes should be correctly populated', () async {
       final dbService = DatabaseService();
       final database = await dbService.database;
       
-      final result = await database.query('roles');
-      final roleNames = result.map((r) => r['name']).toList();
-      
+      final configResults = await database.query('app_configurations');
+      final configKeys = configResults.map((c) => c['config_key']).toList();
+      expect(configKeys, containsAll(['selected_locale_code', 'bg_automation_enabled', 'app_theme_mode']));
+
+      final roleResults = await database.query('roles');
+      final roleNames = roleResults.map((r) => r['name']).toList();
       expect(roleNames, containsAll(['VIP', 'Admin', 'Blocked']));
     });
   });
