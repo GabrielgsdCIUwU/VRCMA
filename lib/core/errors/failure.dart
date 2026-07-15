c89403ad8dca57410e8a1932ed739911f35b0136
GabrielgsdCIUwU <gsuarezdominguez@deloitte.es>
2026-07-15 12:32:55 +0200

    feat(failure): extend Failure class to support Equatable and add DomainFailure

--- lib/core/errors/failure.dart
+++ lib/core/errors/failure.dart
@@ -1,7 +1,13 @@
+import 'package:equatable/equatable.dart';
+import 'package:vrcma/domain/error/domain_exception.dart';
+
 /// Base class for handling application errors.
-abstract class Failure {
+abstract class Failure extends Equatable {
   final String message;
   const Failure(this.message);
+
+  @override
+  List<Object?> get props => [message];
 }
 
 /// Represents errors coming from the VRChat API.
@@ -17,6 +23,9 @@ class DatabaseFailure extends Failure {
 class TwoFactorRequiredFailure extends Failure {
   final List<String> methods;
   const TwoFactorRequiredFailure(this.methods) : super('2FA required');
+
+  @override
+  List<Object?> get props => [message, methods];
 }
 
 
@@ -24,8 +33,20 @@ class RateLimitFailure extends Failure {
   final int retryAfterMinutes;
   const RateLimitFailure(this.retryAfterMinutes)
       : super('Rate limit exceeded. Retry after $retryAfterMinutes minutes.');
+  
+  @override
+  List<Object?> get props => [message, retryAfterMinutes];
 }
 
 class SyncFailure extends Failure {
   const SyncFailure(super.message);
+}
+
+class DomainFailure extends Failure {
+  final DomainException exception;
+
+  DomainFailure(this.exception) : super(exception.defaultMessage);
+
+  @override
+  List<Object?> get props => [message, exception];
 }
